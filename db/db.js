5085cf19adb69aaae3ac0600c63bc45430c70466
victorsalves99 <victorsalves99@outlook.com>
2024-05-03 18:10:48 -0300

    url db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const main = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/livraria").then(() => {
+  await mongoose.connect("mongodb+srv://victoralves:mica1234@cluster0.hcsor8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
     console.log("Conectou!");
   }).catch(() => {
     console.log("Erro ao conectar!");
